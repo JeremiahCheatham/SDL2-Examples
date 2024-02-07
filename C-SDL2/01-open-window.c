028170d745b7b2a9a047d5d60d0dae35489091bc
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2024-02-07 10:05:28 +0000

    Add files via upload

--- C-SDL2/01-open-window.c
+++ C-SDL2/01-open-window.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define WINDOW_TITLE "01 Open Window"
 #define SCREEN_WIDTH 800
@@ -11,8 +11,8 @@ struct Game {
     SDL_Renderer *renderer;
 };
 
+void game_cleanup(struct Game *game, int exit_status);
 bool sdl_initialize(struct Game *game);
-void game_cleanup(struct Game *game);
 
 int main() {
     struct Game game = {
@@ -21,31 +21,25 @@ int main() {
     };
 
     if (sdl_initialize(&game)) {
-        game_cleanup(&game);
+        game_cleanup(&game, EXIT_FAILURE);
     }
 
-    // Update all objects here.
-
-    // Clears the back screen buffer.
     SDL_RenderClear(game.renderer);
 
-    // Do all your drawing here.
-
-    // Flips the front and back buffers, displays what has been drawn.
     SDL_RenderPresent(game.renderer);
 
     SDL_Delay(5000);
 
-    game_cleanup(&game);
+    game_cleanup(&game, EXIT_SUCCESS);
 
     return 0;
 }
 
-void game_cleanup(struct Game *game) {
+void game_cleanup(struct Game *game, int exit_status) {
     SDL_DestroyRenderer(game->renderer);
     SDL_DestroyWindow(game->window);
     SDL_Quit();
-    exit(0);
+    exit(exit_status);
 }
 
 bool sdl_initialize(struct Game *game) {
@@ -54,18 +48,19 @@ bool sdl_initialize(struct Game *game) {
         return true;
     }
 
-    game->window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if (!game->window){
+    game->window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,
+                                    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                                    SCREEN_HEIGHT, 0);
+    if (!game->window) {
         fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
         return true;
     }
 
     game->renderer = SDL_CreateRenderer(game->window, -1, 0);
-    if (!game->renderer){
+    if (!game->renderer) {
         fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
         return true;
     }
 
     return false;
 }
-
