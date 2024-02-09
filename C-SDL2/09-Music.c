3465b65089829d3530cc96418436257d35d36c22
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2024-02-09 02:32:41 +0000

    Add files via upload

--- C-SDL2/09-Music.c
+++ C-SDL2/09-Music.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define WINDOW_TITLE "08 Sound Effects"
+#define WINDOW_TITLE "09 Music"
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define IMAGE_FLAGS IMG_INIT_PNG
