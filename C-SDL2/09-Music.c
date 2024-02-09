b440b82c5bf3b59610668bf8531398fed9a38e11
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2024-02-09 02:29:31 +0000

    Add files via upload

--- C-SDL2/09-Music.c
+++ C-SDL2/09-Music.c
@@ -7,32 +7,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define WINDOW_TITLE "07 Music"
+#define WINDOW_TITLE "08 Sound Effects"
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define IMAGE_FLAGS IMG_INIT_PNG
 #define TEXT_SIZE 80
+#define MIXER_FLAGS MIX_INIT_OGG
 
 struct Game {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *background;
     TTF_Font *text_font;
-    SDL_Texture *text_image;
+    SDL_Color text_color;
     SDL_Rect text_rect;
+    SDL_Texture *text_image;
     int text_xvel;
     int text_yvel;
-    SDL_Color text_color;
     SDL_Texture *sprite_image;
     SDL_Rect sprite_rect;
     int sprite_vel;
     const Uint8 *keystate;
+    Mix_Chunk *c_sound;
+    Mix_Chunk *sdl_sound;
     Mix_Music *music;
 };
 
-bool sdl_initialize(struct Game *game);
+void game_cleanup(struct Game *game, int exit_status);
 bool load_media(struct Game *game);
-void game_cleanup(struct Game *game);
+bool sdl_initialize(struct Game *game);
 void text_update(struct Game *game);
 void sprite_update(struct Game *game);
 
@@ -42,27 +45,31 @@ int main() {
         .renderer = NULL,
         .background = NULL,
         .text_font = NULL,
+        .text_color = {255, 255, 255, 255},
+        .text_rect = {100, 0, 0, 0},
         .text_image = NULL,
-        .text_color = {255, 255, 225, 255},
         .text_xvel = 3,
         .text_yvel = 3,
-        .keystate = SDL_GetKeyboardState(NULL),
         .sprite_image = NULL,
-        .sprite_vel = 3,
+        .sprite_rect = {0, 0, 0, 0},
+        .sprite_vel = 5,
+        .keystate = SDL_GetKeyboardState(NULL),
+        .c_sound = NULL,
+        .sdl_sound = NULL,
         .music = NULL,
     };
 
     if (sdl_initialize(&game)) {
-        game_cleanup(&game);
+        game_cleanup(&game, EXIT_FAILURE);
     }
 
     if (load_media(&game)) {
-        game_cleanup(&game);
+        game_cleanup(&game, EXIT_FAILURE);
     }
 
     if (Mix_PlayMusic(game.music, -1)) {
         fprintf(stderr, "Error playing music: %s\n", Mix_GetError());
-        game_cleanup(&game);
+        return true;
     }
 
     while (true) {
@@ -70,21 +77,23 @@ int main() {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_QUIT:
-                game_cleanup(&game);
+                game_cleanup(&game, EXIT_SUCCESS);
                 break;
             case SDL_KEYDOWN:
                 switch (event.key.keysym.scancode) {
                 case SDL_SCANCODE_ESCAPE:
-                    game_cleanup(&game);
+                    game_cleanup(&game, EXIT_SUCCESS);
+                    break;
                 case SDL_SCANCODE_SPACE:
                     SDL_SetRenderDrawColor(game.renderer, rand() % 256,
                                            rand() % 256, rand() % 256, 255);
+                    Mix_PlayChannel(-1, game.c_sound, 0);
                     break;
                 case SDL_SCANCODE_M:
-                    if (!Mix_PausedMusic()) {
-                        Mix_PauseMusic();
-                    } else {
+                    if (Mix_PausedMusic()) {
                         Mix_ResumeMusic();
+                    } else {
+                        Mix_PauseMusic();
                     }
                     break;
                 default:
@@ -95,44 +104,49 @@ int main() {
             }
         }
 
-        // Update all objects here.
         text_update(&game);
+
         sprite_update(&game);
 
-        // Clears the back screen buffer.
         SDL_RenderClear(game.renderer);
 
-        // Do all your drawing here.
         SDL_RenderCopy(game.renderer, game.background, NULL, NULL);
+
         SDL_RenderCopy(game.renderer, game.text_image, NULL, &game.text_rect);
+
         SDL_RenderCopy(game.renderer, game.sprite_image, NULL,
                        &game.sprite_rect);
 
-        // Flips the front and back buffers, displays what has been drawn.
         SDL_RenderPresent(game.renderer);
 
         SDL_Delay(16);
     }
 
-    game_cleanup(&game);
+    game_cleanup(&game, EXIT_SUCCESS);
 
     return 0;
 }
 
-void game_cleanup(struct Game *game) {
+void game_cleanup(struct Game *game, int exit_status) {
     Mix_HaltMusic();
     Mix_FreeMusic(game->music);
+    Mix_HaltChannel(-1);
+    Mix_FreeChunk(game->sdl_sound);
+    Mix_FreeChunk(game->c_sound);
+    Mix_CloseAudio();
     SDL_DestroyTexture(game->sprite_image);
-    TTF_CloseFont(game->text_font);
     SDL_DestroyTexture(game->text_image);
+    TTF_CloseFont(game->text_font);
     SDL_DestroyTexture(game->background);
     SDL_DestroyRenderer(game->renderer);
     SDL_DestroyWindow(game->window);
+
     Mix_Quit();
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
-    exit(0);
+
+    exit(exit_status);
 }
 
 bool sdl_initialize(struct Game *game) {
@@ -148,12 +162,19 @@ bool sdl_initialize(struct Game *game) {
     }
 
     if (TTF_Init()) {
-        fprintf(stderr, "Error initializing SDL_ttf: %s\n", TTF_GetError());
+        fprintf(stderr, "Error initializing SDL_ttf: %s\n", IMG_GetError());
         return true;
     }
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+    int mix_init = Mix_Init(MIXER_FLAGS);
+    if ((mix_init & MIXER_FLAGS) != MIXER_FLAGS) {
         fprintf(stderr, "Error initializing SDL_mixer: %s\n", Mix_GetError());
+        return true;
+    }
+
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT,
+                      MIX_DEFAULT_CHANNELS, 1024)) {
+        fprintf(stderr, "Error Opening Audio: %s\n", Mix_GetError());
         return true;
     }
 
@@ -171,7 +192,7 @@ bool sdl_initialize(struct Game *game) {
         return true;
     }
 
-    srand((Uint32)time(NULL));
+    srand((unsigned)time(NULL));
 
     return false;
 }
@@ -179,51 +200,58 @@ bool sdl_initialize(struct Game *game) {
 bool load_media(struct Game *game) {
     game->background = IMG_LoadTexture(game->renderer, "images/background.png");
     if (!game->background) {
-        fprintf(stderr, "Error creating a texture: %s\n", IMG_GetError());
+        fprintf(stderr, "Error creating Texture: %s\n", IMG_GetError());
         return true;
     }
 
     game->text_font = TTF_OpenFont("fonts/freesansbold.ttf", TEXT_SIZE);
     if (!game->text_font) {
-        fprintf(stderr, "Error creating a font: %s\n", TTF_GetError());
+        fprintf(stderr, "Error creating Font: %s\n", TTF_GetError());
         return true;
     }
 
     SDL_Surface *surface =
         TTF_RenderText_Blended(game->text_font, "SDL", game->text_color);
     if (!surface) {
-        fprintf(stderr, "Error creating surface from string: %s\n",
-                TTF_GetError());
+        fprintf(stderr, "Error creating Surface: %s\n", SDL_GetError());
         return true;
-    } else {
-        game->text_rect.w = surface->w;
-        game->text_rect.h = surface->h;
-        game->text_image =
-            SDL_CreateTextureFromSurface(game->renderer, surface);
-        SDL_FreeSurface(surface);
-        surface = NULL;
-        if (!game->text_image) {
-            fprintf(stderr, "Error creating texure from surface: %s\n",
-                    SDL_GetError());
-            return true;
-        }
+    }
+    game->text_rect.w = surface->w;
+    game->text_rect.h = surface->h;
+    game->text_image = SDL_CreateTextureFromSurface(game->renderer, surface);
+    SDL_FreeSurface(surface);
+    if (!game->text_image) {
+        fprintf(stderr, "Error creating Texture: %s\n", SDL_GetError());
+        return true;
     }
 
     game->sprite_image = IMG_LoadTexture(game->renderer, "images/C-logo.png");
     if (!game->sprite_image) {
-        fprintf(stderr, "Error creating a texture: %s\n", IMG_GetError());
+        fprintf(stderr, "Error creating Texture: %s\n", IMG_GetError());
         return true;
     }
 
     if (SDL_QueryTexture(game->sprite_image, NULL, NULL, &game->sprite_rect.w,
                          &game->sprite_rect.h)) {
-        fprintf(stderr, "Error querying texture: %s\n", SDL_GetError());
+        fprintf(stderr, "Error querying Texture: %s\n", SDL_GetError());
+        return true;
+    }
+
+    game->c_sound = Mix_LoadWAV("sounds/C.ogg");
+    if (!game->c_sound) {
+        fprintf(stderr, "Error loading Chunk: %s\n", Mix_GetError());
+        return true;
+    }
+
+    game->sdl_sound = Mix_LoadWAV("sounds/SDL.ogg");
+    if (!game->sdl_sound) {
+        fprintf(stderr, "Error loading Chunk: %s\n", Mix_GetError());
         return true;
     }
 
     game->music = Mix_LoadMUS("music/freesoftwaresong-8bit.ogg");
     if (!game->music) {
-        fprintf(stderr, "Failed to load music: %s\n", Mix_GetError());
+        fprintf(stderr, "Error loading Music: %s\n", Mix_GetError());
         return true;
     }
 
@@ -233,17 +261,21 @@ bool load_media(struct Game *game) {
 void text_update(struct Game *game) {
     game->text_rect.x += game->text_xvel;
     game->text_rect.y += game->text_yvel;
-    if (game->text_rect.y + game->text_rect.h > SCREEN_HEIGHT) {
-        game->text_yvel -= game->text_yvel * 2;
-    }
     if (game->text_rect.x + game->text_rect.w > SCREEN_WIDTH) {
-        game->text_xvel -= game->text_xvel * 2;
-    }
-    if (game->text_rect.y < 0) {
-        game->text_yvel -= game->text_yvel * 2;
+        game->text_xvel = -3;
+        Mix_PlayChannel(-1, game->sdl_sound, 0);
     }
     if (game->text_rect.x < 0) {
-        game->text_xvel -= game->text_xvel * 2;
+        game->text_xvel = 3;
+        Mix_PlayChannel(-1, game->sdl_sound, 0);
+    }
+    if (game->text_rect.y + game->text_rect.h > SCREEN_HEIGHT) {
+        game->text_yvel = -3;
+        Mix_PlayChannel(-1, game->sdl_sound, 0);
+    }
+    if (game->text_rect.y < 0) {
+        game->text_yvel = 3;
+        Mix_PlayChannel(-1, game->sdl_sound, 0);
     }
 }
 
